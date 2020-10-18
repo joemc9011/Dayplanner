99ccdb4f1ead0396bf62e627c76c767787af5af8
joemc9011 <joemc9011@gmail.com>
2020-10-18 15:49:02 -0400

    id for text fields and working clock

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,20 @@
-var today = new Date();
-var date = (today.getMonth() + 1)  + "-" +  today.getDate() + "-" + today.getFullYear();
+// var today = new Date();
+// var date = (today.getMonth() + 1)  + "-" +  today.getDate() + "-" + today.getFullYear();
+// var min = "<br>"+(today.getHours() + -12) + ":" + today.getMinutes();
+// $("#currentDay").append(date, min);
 
-
-function running() {
-    var milli = 1000;
-    $("#currentDay").append(date+milli);
+// function running(min) {
+//     var milli = 1000;
+//     newtime=setTimeout(date.running());
     
+// }
+
+// had to research this because I knew there had to be a much cleaner code out there than what I was doing above. Completely forgot about setInterval,
+// but wasn't aware of/didnt remember toLocaleTimeString which I'm assuming takes the new Date info and puts it into a string.
+var myVar = setInterval (myTimer, 1000);
+function myTimer() {
+    var d = new Date();
+    $("#currentDay").text(d.toLocaleTimeString());
 }
 
 
@@ -13,3 +22,11 @@ var morning = ["9", "10", "11", "12"];
 var afternoon = ["1", "2", "3", "4"];
 var day = [morning, afternoon];
 
+for ( var i = 0; i < day.length; i++)
+function color (){
+    if (myTimer()>= day.length[i]){
+        $("#hour").css("background-color", "green")
+
+
+    }
+}
