803c703483c38aee5d98ee8ef6f3668599e49519
joemc9011 <joemc9011@gmail.com>
2020-10-18 15:05:58 -0400

    function for running clock?

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
 var today = new Date();
 var date = (today.getMonth() + 1)  + "-" +  today.getDate() + "-" + today.getFullYear();
-$("#currentDay").append(date);
 
 
+function running() {
+    var milli = 1000;
+    $("#currentDay").append(date+milli);
+    
+}
+
+
+var morning = ["9", "10", "11", "12"];
+var afternoon = ["1", "2", "3", "4"];
+var day = [morning, afternoon];
 
